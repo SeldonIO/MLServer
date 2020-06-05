ec56c31188ea15e0dfe79381b44837c5a2223f86
Adrian Gonzalez-Martin <agm@seldon.io>
2020-06-05 16:20:08 +0100

    Generate pydantic types

--- hack/generate-types.sh
+++ hack/generate-types.sh
@@ -5,11 +5,13 @@ _generate_pb() {
     -I"${ROOT_FOLDER}/proto" \
     --python_out="${ROOT_FOLDER}/oink/grpc" \
     --grpc_python_out="${ROOT_FOLDER}/oink/grpc" \
-    "${ROOT_FOLDER}/proto/service.proto"
+    "${ROOT_FOLDER}/proto/dataplane.proto"
 }
 
 _generate_pydantic() {
-
+  datamodel-codegen \
+    --input "${ROOT_FOLDER}/openapi/dataplane.yaml" \
+    --output "${ROOT_FOLDER}/oink/types.py"
 }
 
 _generate_pb
