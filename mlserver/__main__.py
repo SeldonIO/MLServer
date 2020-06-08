1c54fbe1d7600220235ce49a89ffa3717d072025
Adrian Gonzalez-Martin <agm@seldon.io>
2020-06-08 17:53:16 +0100

    Serve both REST and gRPC

--- mlserver/__main__.py
+++ mlserver/__main__.py
@@ -2,6 +2,7 @@
 Starts an inference server.
 """
 import uvicorn
+import multiprocessing
 
 from .rest import create_app
 from .grpc import create_server
@@ -9,13 +10,33 @@ from .settings import Settings
 from .handlers import DataPlane
 
 
-if __name__ == "__main__":
-    settings = Settings()
-    data_plane = DataPlane()
-
+def start_rest(settings: Settings, data_plane: DataPlane):
     app = create_app(settings, data_plane)
     uvicorn.run(app, port=settings.http_port)
 
-    #  server = create_server(settings, data_plane)
-    #  server.start()
-    #  server.wait_for_termination()
+
+def start_grpc(settings: Settings, data_plane: DataPlane):
+    server = create_server(settings, data_plane)
+    server.start()
+    server.wait_for_termination()
+
+
+def _start_server(target: str, settings: Settings, data_plane: DataPlane):
+    p = multiprocessing.Process(target=target, args=(settings, data_plane))
+    p.start()
+    return p
+
+
+def main():
+    settings = Settings()
+    data_plane = DataPlane()
+
+    rest_process = _start_server(start_rest, settings, data_plane)
+    grpc_process = _start_server(start_grpc, settings, data_plane)
+
+    rest_process.join()
+    grpc_process.join()
+
+
+if __name__ == "__main__":
+    main()
