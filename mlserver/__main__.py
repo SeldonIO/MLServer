5694c11b56f2c6422d5023c645bd63fbfb3ce766
Adrian Gonzalez-Martin <agm@seldon.io>
2020-06-09 12:22:20 +0100

    Remove dummy model from __main__

--- mlserver/__main__.py
+++ mlserver/__main__.py
@@ -10,20 +10,6 @@ from .settings import Settings
 from .handlers import DataPlane
 from .registry import ModelRegistry
 
-from .model import Model
-from .types import InferenceRequest, InferenceResponse, ResponseOutput
-
-
-class DummyModel(Model):
-    def predict(self, payload: InferenceRequest) -> InferenceResponse:
-        total = 0
-        for inp in payload.inputs:
-            print(inp.data)
-            total += sum(inp.data)
-
-        output = ResponseOutput(name="total", shape=[1], datatype="INT32", data=[total])
-        return InferenceResponse(model_name="dummy", id="1", outputs=[output])
-
 
 def start_rest(settings: Settings, data_plane: DataPlane):
     app = create_app(settings, data_plane)
@@ -46,8 +32,6 @@ def main():
     settings = Settings()
     model_registry = ModelRegistry()
     data_plane = DataPlane(model_registry)
-
-    model_registry.load("dummy", DummyModel())
 
     rest_process = _start_server(start_rest, settings, data_plane)
     grpc_process = _start_server(start_grpc, settings, data_plane)
