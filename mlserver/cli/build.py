66f78103e8aec122cf6ead05d290e6de4de6ba65
Adrian Gonzalez-Martin <agm@seldon.io>
2020-07-15 16:56:04 +0100

    Clean up code

--- mlserver/cli/build.py
+++ mlserver/cli/build.py
@@ -5,52 +5,51 @@ import os
 
 from textwrap import dedent
 
+BASE_IMAGE_BLOCK = "FROM registry.access.redhat.com/ubi8/python-38"
+UPGRADE_BLOCK = dedent(
+    """
+    # Upgrade pip
+    RUN pip install --upgrade pip setuptools wheel
+    """
+)
+INSTALL_MLSERVER_BLOCK = dedent(
+    """
+    # Install mlserver dependency
+    # TODO: Install with pip once it's published in PyPi
+    RUN pip install git+https://github.com/seldonio/mlserver#egg=mlserver
+    """
+)
+REQUIREMENTS_TXT_BLOCK = dedent(
+    """
+    # Install requirements.txt
+    COPY requirements.txt .
+    RUN pip install -r requirements.txt
+    """
+)
+COPY_LOCAL_BLOCK = dedent(
+    """
+    # Copy local files
+    COPY . .
+    """
+)
+CMD_BLOCK = 'CMD ["mlserver", "serve", "."]'
+
 
 def generate_dockerfile(folder: str) -> str:
     """
     Generates a Dockerfile to build a Docker image.
     """
-
     blocks = [
-        "FROM registry.access.redhat.com/ubi8/python-38",
-        dedent(
-            """
-            # Upgrade pip
-            RUN pip install --upgrade pip setuptools wheel
-            """
-        ),
-        dedent(
-            """
-            # Install mlserver dependency
-            # TODO: Install with pip once it's published in PyPi
-            RUN pip install git+https://github.com/seldonio/mlserver#egg=mlserver
-            """
-        ),
+        BASE_IMAGE_BLOCK,
+        UPGRADE_BLOCK,
+        INSTALL_MLSERVER_BLOCK,
     ]
 
     # If there is a `requirements.txt` file, install it
     requirements_txt = os.path.join(folder, "requirements.txt")
     if os.path.isfile(requirements_txt):
-        blocks.append(
-            dedent(
-                """
-                # Install requirements.txt
-                COPY requirements.txt .
-                RUN pip install -r requirements.txt
-                """
-            )
-        )
+        blocks.append(REQUIREMENTS_TXT_BLOCK)
 
-    blocks.extend(
-        [
-            dedent(
-                """
-                # Copy local files
-                COPY . .
-                """
-            ),
-            'CMD ["mlserver", "serve", "."]',
-        ]
-    )
+    blocks.extend([COPY_LOCAL_BLOCK, CMD_BLOCK])
 
     return "\n".join(blocks)
