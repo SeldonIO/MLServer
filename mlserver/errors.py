6644079b33ee794af8f81d348706963316eef6dd
Adrian Gonzalez-Martin <agm@seldon.io>
2020-07-24 16:34:39 +0100

    Add InferenceError

--- mlserver/errors.py
+++ mlserver/errors.py
@@ -7,3 +7,8 @@ class ModelNotFound(MLServerError):
     def __init__(self, name: str, version: str):
         msg = f"Model {name} with version {version} not found"
         super().__init__(msg)
+
+
+class InferenceError(MLServerError):
+    def __init__(self, msg):
+        super().__init__(msg)
