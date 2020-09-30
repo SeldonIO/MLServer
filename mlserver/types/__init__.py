5cd41016324972b5b6dac5d89f14672b37c5dc0d
Adrian Gonzalez-Martin <agm@seldon.io>
2020-09-30 16:20:35 +0100

    Re-structure how types are organised and add model_repository types

---
+++ mlserver/types/__init__.py
@@ -0,0 +1,31 @@
+from .dataplane import (
+    MetadataServerResponse,
+    MetadataServerErrorResponse,
+    MetadataTensor,
+    MetadataModelErrorResponse,
+    Parameters,
+    TensorData,
+    RequestOutput,
+    ResponseOutput,
+    InferenceResponse,
+    InferenceErrorResponse,
+    MetadataModelResponse,
+    RequestInput,
+    InferenceRequest,
+)
+
+__all__ = [
+    "MetadataServerResponse",
+    "MetadataServerErrorResponse",
+    "MetadataTensor",
+    "MetadataModelErrorResponse",
+    "Parameters",
+    "TensorData",
+    "RequestOutput",
+    "ResponseOutput",
+    "InferenceResponse",
+    "InferenceErrorResponse",
+    "MetadataModelResponse",
+    "RequestInput",
+    "InferenceRequest",
+]
