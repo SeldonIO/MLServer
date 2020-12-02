4bcea51588bd3c6c184438a7052a1b2b29577956
Adrian Gonzalez-Martin <agm@seldon.io>
2020-12-02 15:52:30 +0100

    Add script to bump versions

--- hack/generate-types.sh
+++ hack/generate-types.sh
@@ -1,34 +1,34 @@
 ROOT_FOLDER="$(dirname "${0}")/.."
 
-_generate_pb() {
-  local _api_name=$1
+_generatePB() {
+  local apiName=$1
 
   python -m grpc_tools.protoc \
     -I"${ROOT_FOLDER}/proto" \
     --python_out="${ROOT_FOLDER}/mlserver/grpc" \
     --grpc_python_out="${ROOT_FOLDER}/mlserver/grpc" \
     --mypy_out="${ROOT_FOLDER}/mlserver/grpc" \
-    "${ROOT_FOLDER}/proto/$_api_name.proto"
+    "${ROOT_FOLDER}/proto/$apiName.proto"
 
   # Change to relative import
   # https://github.com/protocolbuffers/protobuf/issues/1491
-  sed -i "s/import $_api_name/from . import $_api_name/" \
-    "${ROOT_FOLDER}/mlserver/grpc/${_api_name}_pb2_grpc.py"
+  sed -i "s/import $apiName/from . import $apiName/" \
+    "${ROOT_FOLDER}/mlserver/grpc/${apiName}_pb2_grpc.py"
 }
 
-_generate_pydantic() {
-  local _api_name=$1
+_generatePydantic() {
+  local apiName=$1
 
   datamodel-codegen \
-    --input "${ROOT_FOLDER}/openapi/$_api_name.yaml" \
-    --output "${ROOT_FOLDER}/mlserver/types/$_api_name.py" \
+    --input "${ROOT_FOLDER}/openapi/$apiName.yaml" \
+    --output "${ROOT_FOLDER}/mlserver/types/$apiName.py" \
     --custom-template-dir "${ROOT_FOLDER}/hack/templates" \
     --disable-timestamp \
     --target-python-version 3.6
 }
 
-_generate_pb dataplane
-_generate_pydantic dataplane
+_generatePB dataplane
+_generatePydantic dataplane
 
-_generate_pb model_repository
-_generate_pydantic model_repository
+_generatePB model_repository
+_generatePydantic model_repository
---
+++ hack/update-version.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+ROOT_FOLDER="$(dirname "${0}")/.."
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: ./update-version.sh <newVersion>"
+  exit 1
+fi
+
+_updateVersion() {
+  local _newVersion=$1
+  local _versionPy=$2
+
+  sed -i "s/^__version__ = \"\(.*\)\"$/__version__ = \"$_newVersion\"/" "$_versionPy"
+}
+
+_main() {
+  local _newVersion=$1
+
+  # To call within `-exec`
+  export -f _updateVersion
+
+  find $ROOT_FOLDER \
+    -type f -name version.py \
+    \( \
+    -path "$ROOT_FOLDER/mlserver/*" -or \
+    -path "$ROOT_FOLDER/runtimes/**/*" \
+    \) \
+    -exec bash -c "_updateVersion $_newVersion {}" \;
+}
+
+_main $1
