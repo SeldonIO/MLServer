67ee756f5dc19cab23ffd4a306672a2bc2121e81
Adrian Gonzalez-Martin <agm@seldon.io>
2021-02-18 14:27:04 +0000

    Load from folder well-known tempo filenames

--- runtimes/tempo/mlserver_tempo/tempo.py
+++ runtimes/tempo/mlserver_tempo/tempo.py
@@ -4,10 +4,14 @@ from mlserver import MLModel
 from mlserver.utils import get_model_uri
 from mlserver.types import InferenceRequest, InferenceResponse
 
+WELLKNOWN_MODEL_FILENAMES = ["model.pickle", "pipeline.pickle"]
+
 
 class TempoModel(MLModel):
     async def load(self) -> bool:
-        pipeline_uri = await get_model_uri(self._settings)
+        pipeline_uri = await get_model_uri(
+            self._settings, wellknown_filenames=WELLKNOWN_MODEL_FILENAMES
+        )
         self._pipeline = Pipeline.load(pipeline_uri)
 
         self.ready = True
