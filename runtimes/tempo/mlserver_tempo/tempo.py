35feab50e5228b431a58f172c4c9898f739da680
Adrian Gonzalez-Martin <agm@seldon.io>
2021-02-18 14:15:18 +0000

    Remove restriction on number of inputs

--- runtimes/tempo/mlserver_tempo/tempo.py
+++ runtimes/tempo/mlserver_tempo/tempo.py
@@ -1,7 +1,6 @@
 from tempo.serve.pipeline import Pipeline
 
 from mlserver import MLModel
-from mlserver.errors import InferenceError
 from mlserver.utils import get_model_uri
 from mlserver.types import InferenceRequest, InferenceResponse
 
@@ -9,26 +8,11 @@ from mlserver.types import InferenceRequest, InferenceResponse
 class TempoModel(MLModel):
     async def load(self) -> bool:
         pipeline_uri = await get_model_uri(self._settings)
-
         self._pipeline = Pipeline.load(pipeline_uri)
 
         self.ready = True
         return self.ready
 
     async def predict(self, payload: InferenceRequest) -> InferenceResponse:
-        payload = self._check_request(payload)
-
-        payload_dict = payload.dict()
-
-        prediction = self._pipeline.request(payload_dict)
-
+        prediction = self._pipeline.request(payload.dict())
         return InferenceResponse(**prediction)
-
-    def _check_request(self, payload: InferenceRequest) -> InferenceRequest:
-        if len(payload.inputs) != 1:
-            raise InferenceError(
-                "TempoModel only supports a single input tensor "
-                f"({len(payload.inputs)} were received)"
-            )
-
-        return payload
--- runtimes/tempo/tests/test_tempo.py
+++ runtimes/tempo/tests/test_tempo.py
@@ -1,8 +1,5 @@
-import pytest
-
 from tempo.serve.pipeline import Pipeline
-from mlserver.types import InferenceRequest, RequestInput
-from mlserver.errors import InferenceError
+from mlserver.types import InferenceRequest
 from mlserver.utils import to_ndarray
 
 from mlserver_tempo import TempoModel
@@ -11,17 +8,6 @@ from mlserver_tempo import TempoModel
 def test_load(model: TempoModel):
     assert model.ready
     assert isinstance(model._pipeline, Pipeline)
-
-
-async def test_multiple_inputs_error(
-    model: TempoModel, inference_request: InferenceRequest
-):
-    inference_request.inputs.append(
-        RequestInput(name="input-1", shape=[2], data=[0, 1], datatype="FP32")
-    )
-
-    with pytest.raises(InferenceError):
-        await model.predict(inference_request)
 
 
 async def test_predict(
