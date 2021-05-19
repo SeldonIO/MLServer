d993bddcdefbb85a4f43a5f56131e24b32308197
Adrian Gonzalez-Martin <agm@seldon.io>
2021-05-19 15:41:48 +0100

    Use self.decode in runtimes

--- runtimes/lightgbm/mlserver_lightgbm/lightgbm.py
+++ runtimes/lightgbm/mlserver_lightgbm/lightgbm.py
@@ -30,10 +30,9 @@ class LightGBMModel(MLModel):
     async def predict(self, payload: types.InferenceRequest) -> types.InferenceResponse:
         payload = self._check_request(payload)
 
-        # _check_request will convert the data to `np.ndarray`
         model_input = payload.inputs[0]
-        data = model_input.parameters["data"]  # type: ignore
-        prediction = self._model.predict(data)
+        decoded = self.decode(model_input, default_codec=self._codec)
+        prediction = self._model.predict(decoded)
 
         # TODO: Set datatype (cast from numpy?)
         return types.InferenceResponse(
@@ -48,16 +47,5 @@ class LightGBMModel(MLModel):
                 "LightGBM only supports a single input tensor "
                 f"({len(payload.inputs)} were received)"
             )
-
-        # Convert to `numpy.ndarray` and store in parameters
-        try:
-            model_input = payload.inputs[0]
-            array_data = self._codec.decode(model_input)
-
-            model_input.parameters = {"data": array_data}  # type: ignore
-        except Exception as e:
-            # There are a few things that can go wrong here, e.g. less than 2-D
-            # in the array), or input data not compatible with a numpy array
-            raise InferenceError("Invalid input to LightGBM") from e
 
         return payload
--- runtimes/xgboost/mlserver_xgboost/xgboost.py
+++ runtimes/xgboost/mlserver_xgboost/xgboost.py
@@ -30,12 +30,10 @@ class XGBoostModel(MLModel):
     async def predict(self, payload: types.InferenceRequest) -> types.InferenceResponse:
         payload = self._check_request(payload)
 
-        # _check_request will convert the data to `xgboost.DMatrix`
         model_input = payload.inputs[0]
-        dmatrix_data = model_input.parameters["dmatrix_data"]  # type: ignore
-        prediction = self._model.predict(dmatrix_data)
+        decoded = self.decode(model_input, default_codec=self._codec)
+        prediction = self._model.predict(decoded)
 
-        # TODO: Set datatype (cast from numpy?)
         return types.InferenceResponse(
             model_name=self.name,
             model_version=self.version,
@@ -48,18 +46,5 @@ class XGBoostModel(MLModel):
                 "XGBoostModel only supports a single input tensor "
                 f"({len(payload.inputs)} were received)"
             )
-
-        # Convert to `xgboost.DMatrix` and store in parameters
-        # TODO: Move this out to "types conversion" pipeline, once it's there.
-        try:
-            model_input = payload.inputs[0]
-            dmatrix_data = self._codec.decode(model_input)
-
-            # TODO: Use Parameters object
-            model_input.parameters = {"dmatrix_data": dmatrix_data}  # type: ignore
-        except Exception as e:
-            # There are a few things that can go wrong here, e.g. less than 2-D
-            # in the array), or input data not compatible with a numpy array
-            raise InferenceError("Invalid input to XGBoostModel") from e
 
         return payload
