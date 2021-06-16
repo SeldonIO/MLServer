80b56864d6ef426a408024e18260897c564bf835
Adrian Gonzalez-Martin <agm@seldon.io>
2021-06-16 12:19:33 +0100

    Make string codec more robust to batches

--- tests/codecs/test_string.py
+++ tests/codecs/test_string.py
@@ -5,20 +5,69 @@ from mlserver.types import RequestInput
 
 
 @pytest.mark.parametrize(
-    "encoded, payload",
+    "request_input, expected",
     [
-        (b"hello world", "hello world"),
+        (
+            RequestInput(name="foo", datatype="BYTES", shape=[], data=b"hello world"),
+            ["hello world"],
+        ),
+        (
+            RequestInput(name="foo", datatype="BYTES", shape=[], data="hello world"),
+            ["hello world"],
+        ),
+        (
+            RequestInput(name="foo", datatype="BYTES", shape=[], data=["hello world"]),
+            ["hello world"],
+        ),
+        (
+            RequestInput(name="foo", datatype="BYTES", shape=[], data=[b"hello world"]),
+            ["hello world"],
+        ),
+        (
+            RequestInput(
+                name="foo", datatype="BYTES", shape=[1, 11], data=b"hello world"
+            ),
+            ["hello world"],
+        ),
+        (
+            RequestInput(
+                name="foo",
+                datatype="BYTES",
+                shape=[2, 11],
+                data=["hello world", "hello world"],
+            ),
+            ["hello world", "hello world"],
+        ),
+        (
+            RequestInput(name="foo", datatype="BYTES", shape=[2, 3], data=b"heyabc"),
+            ["hey", "abc"],
+        ),
     ],
 )
-def test_string_codec(encoded, payload):
-    request_input = RequestInput(name="foo", shape=[], datatype="BYTES", data=encoded)
+def test_decode(request_input, expected):
     codec = StringCodec()
     decoded = codec.decode(request_input)
 
-    assert decoded == payload
+    assert decoded == expected
 
+
+@pytest.mark.parametrize(
+    "decoded, expected",
+    [
+        (
+            ["hello world"],
+            RequestInput(
+                name="foo", shape=[1, 11], datatype="BYTES", data=b"hello world"
+            ),
+        ),
+        (
+            ["hey", "abc"],
+            RequestInput(name="foo", shape=[2, 3], datatype="BYTES", data=b"heyabc"),
+        ),
+    ],
+)
+def test_encode(decoded, expected):
+    codec = StringCodec()
     response_output = codec.encode(name="foo", payload=decoded)
 
-    assert response_output.datatype == "BYTES"
-    assert response_output.shape == [len(encoded)]
-    assert response_output.data.__root__ == encoded
+    assert expected == response_output
