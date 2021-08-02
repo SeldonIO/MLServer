d98c42fbcdd6e10f483485590850dbe830cc0e00
Alejandro Saucedo <axsauze@gmail.com>
2021-08-02 21:55:43 +0100

    Reverted ce

--- mlserver/middleware.py
+++ mlserver/middleware.py
@@ -1,11 +1,8 @@
-from typing import Callable, List, Any
-from fastapi import FastAPI
-from starlette.middleware.base import BaseHTTPMiddleware
+from typing import Callable, List
 
 #  from .codecs.middleware import codec_middleware
 from .settings import ModelSettings
 from .types import InferenceRequest
-from .cloudevents import cloudevents_middleware
 
 MiddlewareFunc = Callable[[InferenceRequest, ModelSettings], InferenceRequest]
 #  InferenceMiddlewares: List[MiddlewareFunc] = [codec_middleware]
@@ -21,18 +18,3 @@ def inference_middlewares(
         request = middleware(request, model_settings)
 
     return request
-
-
-FastAPIMiddlewareFunc = Any
-FastAPIMiddlewares: List[FastAPIMiddlewareFunc] = [cloudevents_middleware]
-
-
-def add_fastapi_middleware(app: FastAPI):
-    """
-    Inference middlewares get called as part of DataPlane.infer
-    as opposed to as a FastAPI middleware given that the expected
-    parameters are the post-processed ones as InferenceResponse /
-    InferenceResponse as opposed to raw requests
-    """
-    for middleware in FastAPIMiddlewares:
-        app.add_middleware(BaseHTTPMiddleware, dispatch=middleware)
