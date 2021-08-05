b42d8b63816a9df502d2b5d2647f885741180bb6
Adrian Gonzalez-Martin <agm@seldon.io>
2021-08-05 15:10:45 +0200

    Bump version to 0.4.1.dev1

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ html_title = "MLServer"
 author = "Seldon Technologies"
 
 # The full version, including alpha/beta/rc tags
-release = "0.4.0"
+release = "0.4.1.dev1"
 
 
 # -- General configuration ---------------------------------------------------
