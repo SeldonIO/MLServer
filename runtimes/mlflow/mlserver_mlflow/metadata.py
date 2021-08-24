90d8d616c33b8e548d009f025cb50fd941f14334
Adrian Gonzalez-Martin <agm@seldon.io>
2021-08-24 15:35:08 +0200

    Add tests for metadata

--- runtimes/mlflow/mlserver_mlflow/metadata.py
+++ runtimes/mlflow/mlserver_mlflow/metadata.py
@@ -1,8 +1,8 @@
-from typing import Union, Tuple
+from typing import Union, Tuple, List
 
-from mlflow.types.schema import ColSpec, TensorSpec, DataType
+from mlflow.types.schema import Schema, ColSpec, TensorSpec, DataType
 from mlserver.types import MetadataTensor, Tags
-from mlserver.codecs import NumpyCodec, StringCodec
+from mlserver.codecs import NumpyCodec, StringCodec, Base64Codec, DatetimeCodec
 from mlserver.codecs.numpy import to_datatype
 
 InputSpec = Union[ColSpec, TensorSpec]
@@ -14,23 +14,40 @@ _MLflowToContentType = {
     DataType.float: ("FP32", NumpyCodec.ContentType),
     DataType.double: ("FP64", NumpyCodec.ContentType),
     DataType.string: ("BYTES", StringCodec.ContentType),
-    # TODO: Add Base64 codec
-    #  DataType.binary: ("BYTES", Base64Codec.ContentType),
-    # TODO: Add DateTimeCodec
-    #  DataType.datetime: ("BYTES", DateTimeCodec.ContentType),
+    DataType.binary: ("BYTES", Base64Codec.ContentType),
+    DataType.datetime: ("BYTES", DatetimeCodec.ContentType),
 }
 
 
-def to_content_type(input_spec: InputSpec) -> Tuple[str, str]:
+def _get_content_type(input_spec: InputSpec) -> Tuple[str, str]:
     if isinstance(input_spec, TensorSpec):
         datatype = to_datatype(input_spec.type)
         content_type = NumpyCodec.ContentType
         return datatype, content_type
 
+    # TODO: Check if new type, which may not exist
+    return _MLflowToContentType[input_spec.type]
+
+
+def _get_shape(input_spec: InputSpec) -> List[int]:
+    if isinstance(input_spec, TensorSpec):
+        return list(input_spec.shape)
+
+    return [-1]
+
 
 def to_metadata_tensor(input_spec: InputSpec) -> MetadataTensor:
     # TODO: Can input_spec.name be None?
+    datatype, content_type = _get_content_type(input_spec)
+    shape = _get_shape(input_spec)
 
-    datatype, tags = to_content_type(input_spec)
+    return MetadataTensor(
+        name=input_spec.name,
+        datatype=datatype,
+        shape=shape,
+        tags=Tags(content_type=content_type),
+    )
 
-    return MetadataTensor(name=input_spec.name, datatype="", shape=[], tags=Tags())
+
+def to_metadata(schema: Schema) -> List[MetadataTensor]:
+    return [to_metadata_tensor(schema_input) for schema_input in schema.inputs]
--- runtimes/mlflow/tests/test_metadata.py
+++ runtimes/mlflow/tests/test_metadata.py
@@ -1,23 +1,65 @@
 import pytest
 import numpy as np
 
-from typing import Tuple
+from typing import Tuple, List
 
 from mlflow.types.schema import ColSpec, TensorSpec, DataType
-from mlserver.codecs import NumpyCodec
+from mlserver.codecs import NumpyCodec, StringCodec, Base64Codec
+from mlserver.types import Tags
 
-from mlserver_mlflow.metadata import InputSpec, to_content_type
+from mlserver_mlflow.metadata import (
+    InputSpec,
+    _get_content_type,
+    _get_shape,
+    to_metadata_tensor,
+)
 
 
 @pytest.mark.parametrize(
     "input_spec, expected",
     [
         (
-            TensorSpec(type=np.dtype("int32"), shape=(2, 2), name="foo"),
+            TensorSpec(name="foo", shape=(2, 2), type=np.dtype("int32")),
             ("INT32", NumpyCodec.ContentType),
-        )
+        ),
+        (
+            ColSpec(name="foo", type=DataType.string),
+            ("BYTES", StringCodec.ContentType),
+        ),
+        (
+            ColSpec(name="foo", type=DataType.binary),
+            ("BYTES", Base64Codec.ContentType),
+        ),
     ],
 )
-def test_to_content_type(input_spec: InputSpec, expected: Tuple[str, str]):
-    datatype, content_type = to_content_type(input_spec)
+def test_get_content_type(input_spec: InputSpec, expected: Tuple[str, str]):
+    datatype, content_type = _get_content_type(input_spec)
     assert (datatype, content_type) == expected
+
+
+@pytest.mark.parametrize(
+    "input_spec, expected",
+    [
+        (
+            TensorSpec(name="foo", shape=(2, 2), type=np.dtype("int32")),
+            [2, 2],
+        ),
+        (
+            ColSpec(name="foo", type=DataType.string),
+            [-1],
+        ),
+    ],
+)
+def test_get_shape(input_spec: InputSpec, expected: List[int]):
+    shape = _get_shape(input_spec)
+    assert shape == expected
+
+
+def test_to_metadata_tensor():
+    input_spec = TensorSpec(name="foo", shape=(2, 2), type=np.dtype("int32"))
+    metadata_tensor = to_metadata_tensor(input_spec)
+
+    assert metadata_tensor.name == input_spec.name
+    assert metadata_tensor.datatype == "INT32"
+    assert metadata_tensor.shape == list(input_spec.shape)
+    assert metadata_tensor.tags == Tags(content_type=NumpyCodec.ContentType)
