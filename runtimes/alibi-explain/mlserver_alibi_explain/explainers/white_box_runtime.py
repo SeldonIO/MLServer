e525b56c55f22c8de6d62048e0d1efdc79619849
Sherif Akoush <sa@seldon.io>
2021-10-15 12:38:30 +0100

    and for whitebox

--- runtimes/alibi-explain/mlserver_alibi_explain/explainers/white_box_runtime.py
+++ runtimes/alibi-explain/mlserver_alibi_explain/explainers/white_box_runtime.py
@@ -5,6 +5,7 @@ from alibi.api.interfaces import Explainer
 from alibi.saving import load_explainer
 
 from mlserver import ModelSettings
+from mlserver.errors import ModelParametersMissing, InvalidModelURI
 from mlserver.settings import ModelParameters
 from mlserver_alibi_explain.common import AlibiExplainSettings
 from mlserver_alibi_explain.runtime import AlibiExplainRuntimeBase
@@ -38,10 +39,16 @@ class AlibiExplainWhiteBoxRuntime(ABC, AlibiExplainRuntimeBase):
         else:
             # load the model from disk
             # full model is passed as `predictor`
+            # load the model from disk
             model_parameters: Optional[ModelParameters] = self.settings.parameters
-            assert model_parameters is not None
-            uri = model_parameters.uri  # type ignore
-            assert uri is not None, "uri has to be set"
+            if model_parameters is None:
+                raise ModelParametersMissing(self.name)
+
+            uri = model_parameters.uri
+
+            if uri is None:
+                raise InvalidModelURI(self.name)
+
             self._model = load_explainer(uri, predictor=self._inference_model)
 
         self.ready = True
