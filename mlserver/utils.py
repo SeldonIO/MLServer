afdd13be016ffcac2454e90f57cc899ab074b5b0
Adrian Gonzalez-Martin <agm@seldon.io>
2021-11-12 14:49:59 +0000

    CHange var name

--- mlserver/utils.py
+++ mlserver/utils.py
@@ -13,34 +13,36 @@ async def get_model_uri(
     if not settings.parameters:
         raise InvalidModelURI(settings.name)
 
-    relative_model_uri = settings.parameters.uri
-    if not relative_model_uri:
+    model_uri = settings.parameters.uri
+    if not model_uri:
         raise InvalidModelURI(settings.name)
 
-    model_uri = _to_absolut_path(settings._source, relative_model_uri)
-    if os.path.isfile(model_uri):
-        return model_uri
+    full_model_uri = _to_absolute_path(settings._source, model_uri)
+    if os.path.isfile(full_model_uri):
+        return full_model_uri
 
-    if os.path.isdir(model_uri):
-        # If model_uri is a folder, search for a well-known model filename
+    if os.path.isdir(full_model_uri):
+        # If full_model_uri is a folder, search for a well-known model filename
         for fname in wellknown_filenames:
-            model_path = os.path.join(model_uri, fname)
+            model_path = os.path.join(full_model_uri, fname)
             if os.path.isfile(model_path):
                 return model_path
 
         # If none, return the folder
-        return model_uri
+        return full_model_uri
 
     # Otherwise, the uri is neither a file nor a folder
-    raise InvalidModelURI(settings.name, model_uri)
+    raise InvalidModelURI(settings.name, full_model_uri)
 
 
-def _to_absolut_path(source: Optional[str], relative_model_uri: str) -> str:
+def _to_absolute_path(source: Optional[str], model_uri: str) -> str:
     if source is None:
-        return relative_model_uri
+        # Treat path as either absolute or relative to the working directory of
+        # the MLServer instance
+        return model_uri
 
     parent_folder = os.path.dirname(source)
-    unnormalised = os.path.join(parent_folder, relative_model_uri)
+    unnormalised = os.path.join(parent_folder, model_uri)
     return os.path.normpath(unnormalised)
 
 
