98f9976e3f3b629346f6677c00dcf526e638e69b
Adrian Gonzalez-Martin <agm@seldon.io>
2021-11-17 14:36:29 +0000

    Add sphinx-panels dep

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,7 @@ extensions = [
     "sphinxcontrib.bibtex",
     "sphinxcontrib.autodoc_pydantic",
     "sphinx_click",
+    "sphinx_panels",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
