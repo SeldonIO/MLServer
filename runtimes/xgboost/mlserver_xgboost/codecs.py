6df471023d1abd25b9c0281068576b8d97d1ce86
Adrian Gonzalez-Martin <agm@seldon.io>
2021-11-26 16:44:19 +0000

    Migrated XGBoost runtime

--- runtimes/xgboost/mlserver_xgboost/codecs.py
+++ runtimes/xgboost/mlserver_xgboost/codecs.py
@@ -1,12 +1,17 @@
 from xgboost import DMatrix
 
-from mlserver.codecs import NumpyCodec
+from mlserver.codecs import NumpyCodec, register_input_codec, register_request_codec
+from mlserver.codecs.utils import FirstInputRequestCodec
 from mlserver.types import RequestInput
 from mlserver.errors import InferenceError
 
 
+@register_input_codec
 class DMatrixCodec(NumpyCodec):
-    def decode(self, request_input: RequestInput) -> DMatrix:  # type: ignore
+    ContentType = "dmatrix"
+
+    @classmethod
+    def decode(cls, request_input: RequestInput) -> DMatrix:  # type: ignore
         try:
             ndarray = super().decode(request_input)
             return DMatrix(ndarray)
@@ -14,3 +19,9 @@ class DMatrixCodec(NumpyCodec):
             # There are a few things that can go wrong here, e.g. less than 2-D
             # in the array), or input data not compatible with a numpy array
             raise InferenceError("Invalid input to XGBoostModel") from e
+
+
+@register_request_codec
+class DMatrixRequestCodec(FirstInputRequestCodec):
+    InputCodec = DMatrixCodec
+    ContentType = DMatrixCodec.ContentType
