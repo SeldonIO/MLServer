b206cbd2c4eec3dee65f01c9b43115693332153d
joerunde <filbertrunde@gmail.com>
2022-01-11 10:35:33 -0700

    ♻️Extract SKLearn encoding logic

--- runtimes/sklearn/mlserver_sklearn/sklearn.py
+++ runtimes/sklearn/mlserver_sklearn/sklearn.py
@@ -1,17 +1,15 @@
+from typing import List
+
 import joblib
-
-from typing import List, Tuple
-
-import pandas as pd
-from pandas import DataFrame
+from mlserver_sklearn import encoding
+from mlserver_sklearn.encoding import SKLearnPayload
 from sklearn.pipeline import Pipeline
 
 from mlserver import types
-from mlserver.model import MLModel
+from mlserver.codecs import NumpyRequestCodec
 from mlserver.errors import InferenceError
-from mlserver.types import ResponseOutput, RequestOutput
+from mlserver.model import MLModel
 from mlserver.utils import get_model_uri
-from mlserver.codecs import NumpyCodec, NumpyRequestCodec, PandasCodec
 
 PREDICT_OUTPUT = "predict"
 PREDICT_PROBA_OUTPUT = "predict_proba"
@@ -41,19 +39,7 @@ class SKLearnModel(MLModel):
 
         model_responses = self._get_model_outputs(payload)
 
-        if (len(model_responses) == 1 and isinstance(model_responses[0][1], DataFrame)):
-            print("Trying to encode data frame")
-            df: DataFrame = model_responses[0][1]
-            return PandasCodec.encode(model_name=self.name,
-                                      model_version=self.version,
-                                      payload=df)
-
-        print("Not encoding dataframe")
-        return types.InferenceResponse(
-                model_name=self.name,
-                model_version=self.version,
-                outputs=self._encode_outputs(model_responses),
-            )
+        return encoding.to_response(self.name, self.version, model_responses)
 
     def _check_request(self, payload: types.InferenceRequest) -> types.InferenceRequest:
         if not payload.outputs:
@@ -83,30 +69,14 @@ class SKLearnModel(MLModel):
 
         return payload
 
-    def _get_model_outputs(self, payload: types.InferenceRequest) -> List[Tuple[RequestOutput, object]]:
+    def _get_model_outputs(self, payload: types.InferenceRequest) -> List[SKLearnPayload]:
         decoded_request = self.decode_request(payload, default_codec=NumpyRequestCodec)
 
         outputs = []
         for request_output in payload.outputs:  # type: ignore
             predict_fn = getattr(self._model, request_output.name)
             y = predict_fn(decoded_request)
-            outputs.append((request_output, y))
+            payload = SKLearnPayload(requested_output=request_output, model_output=y)
+            outputs.append(payload)
 
         return outputs
-
-    def _encode_outputs(self, outputs: List[Tuple[RequestOutput, object]]) -> List[ResponseOutput]:
-        response_outputs = []
-
-        all_output_names = [o[0].name for o in outputs]
-
-        for output, response in outputs:
-            if isinstance(response, pd.DataFrame):
-                raise InferenceError(f"{output.name} is of type DataFrame and {all_output_names} were"
-                                     f" requested. Cannot encode multiple DataFrames in one response."
-                                     f" Please request only a single DataFrame output.")
-
-            # TODO: accommodate more things like Strings
-            response_output = NumpyCodec.encode(name=output.name, payload=response)
-            response_outputs.append(response_output)
-
-        return response_outputs
