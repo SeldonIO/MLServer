f24ef138b19b4365fbbd2a9fc363eeca2734c14c
joerunde <filbertrunde@gmail.com>
2022-01-10 17:35:49 -0700

    🐛 Fix py37 pickle compatibility bug

--- runtimes/sklearn/tests/conftest.py
+++ runtimes/sklearn/tests/conftest.py
@@ -9,6 +9,7 @@ from sklearn.dummy import DummyClassifier, DummyRegressor
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.base import BaseEstimator
+from .dummy_model import DummyDataframeModel
 
 from mlserver.settings import ModelSettings, ModelParameters
 from mlserver.types import InferenceRequest
@@ -176,24 +177,6 @@ async def string_model(tmp_path) -> SKLearnModel:
     model = SKLearnModel(dummy_model_settings)
     await model.load()
     return model
-
-
-class DummyDataframeModel(BaseEstimator):
-    """predict/_proba return data frames"""
-
-    def predict(self, X):
-        frame = pd.DataFrame()
-        frame["label_1"] = np.array([1])
-        frame["label_2"] = np.array([2])
-        frame["label_3"] = 3
-        return frame
-
-    def predict_proba(self, X):
-        frame = pd.DataFrame()
-        frame["label_1_prob"] = np.array([0.123])
-        frame["label_2_prob"] = np.array([0.456])
-        frame["label_3_prob"] = 0.789
-        return frame
 
 @pytest.fixture
 async def dataframe_model(tmp_path) -> SKLearnModel:
