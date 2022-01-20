d27df20d38fbf93e55e17d45849b409e0f34299f
Adrian Gonzalez-Martin <agm@seldon.io>
2022-01-20 12:33:04 +0100

    Add type hint

--- tests/handlers/conftest.py
+++ tests/handlers/conftest.py
@@ -2,7 +2,9 @@ import pytest
 
 from mlserver.handlers.custom import CustomHandler
 
+from ..fixtures import SumModel
+
 
 @pytest.fixture
-def custom_handler() -> CustomHandler:
+def custom_handler(sum_model: SumModel) -> CustomHandler:
     return CustomHandler(rest_path="/my-custom-endpoint")
