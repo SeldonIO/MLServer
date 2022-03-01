cebfb82beb2ff17b65bb6fe97ede32f9230fa716
Adrian Gonzalez-Martin <agm@seldon.io>
2022-03-01 17:28:13 +0000

    Use allclose instance of array_equal

--- runtimes/alibi-explain/tests/test_black_box.py
+++ runtimes/alibi-explain/tests/test_black_box.py
@@ -7,7 +7,7 @@ import numpy as np
 import pytest
 import tensorflow as tf
 from alibi.saving import load_explainer
-from numpy.testing import assert_array_equal
+from numpy.testing import assert_allclose
 
 from helpers.tf_model import get_tf_mnist_model_uri
 from mlserver import MLModel
@@ -80,7 +80,7 @@ async def test_predict_impl(
         expected_result.outputs[0]
     )
 
-    assert_array_equal(actual_result, expected_result_numpy)
+    assert_allclose(actual_result, expected_result_numpy)
 
 
 @pytest.fixture()
@@ -108,7 +108,7 @@ async def test_end_2_end(
         # we remove it for alibi
     )
 
-    assert_array_equal(
+    assert_allclose(
         np.array(decoded_runtime_results["data"]["anchor"]), alibi_result.data["anchor"]
     )
 
