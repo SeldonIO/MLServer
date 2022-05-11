f6ea2394f71827f5caeee3afb15d3f8cf7817670
Alejandro Saucedo <axsauze@gmail.com>
2022-05-11 09:35:49 +0100

    Updating HuggingFace Optimum library to v1.2.0 (#580)

--- runtimes/huggingface/setup.py
+++ runtimes/huggingface/setup.py
@@ -35,10 +35,7 @@ setup(
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=[
         "mlserver",
-        "optimum[onnxruntime]",
-    ],
-    dependency_links=[
-        "git+https://github.com/huggingface/optimum.git#egg=optimum[onnxruntime]"
+        "optimum[onnxruntime]==1.2.0",
     ],
     long_description=_load_description(),
     long_description_content_type="text/markdown",
