9b1a64b2a744cd3ec3279dedb8c2ba405b75e323
Adrian Gonzalez-Martin <agm@seldon.io>
2022-06-01 16:03:27 +0100

    Add deprecation note

--- mlserver/grpc/model_repository.py
+++ mlserver/grpc/model_repository.py
@@ -1,3 +1,5 @@
+from typing import Callable
+
 from . import model_repository_pb2 as mr_pb
 from .converters import (
     RepositoryIndexRequestConverter,
@@ -5,14 +7,30 @@ from .converters import (
 )
 from .model_repository_pb2_grpc import ModelRepositoryServiceServicer
 from .utils import handle_mlserver_error
+from .logging import logger
 from ..handlers import ModelRepositoryHandlers
+
+
+def _deprecated(f: Callable):
+    async def _inner(self, request, context):
+        logger.warning(
+            "DEPRECATED!! "
+            f"The `inference.model_repository.ModelRepositoryService/{f.__name__}` "
+            "endpoint has now been deprecated and will be removed in "
+            "MLServer 1.2.0. "
+            f"Please use the `inference.GRPCInferenceService/{f.__name__}` "
+            "endpoint instead."
+        )
+        return await f(self, request, context)
+
+    return _inner
 
 
 class ModelRepositoryServicer(ModelRepositoryServiceServicer):
     def __init__(self, handlers: ModelRepositoryHandlers):
         self._handlers = handlers
 
-    # TODO: Add deprecation note
+    @_deprecated
     async def RepositoryIndex(
         self, request: mr_pb.RepositoryIndexRequest, context
     ) -> mr_pb.RepositoryIndexResponse:
@@ -23,6 +41,7 @@ class ModelRepositoryServicer(ModelRepositoryServiceServicer):
         )
 
     @handle_mlserver_error
+    @_deprecated
     async def RepositoryModelLoad(
         self, request: mr_pb.RepositoryModelLoadRequest, context
     ) -> mr_pb.RepositoryModelLoadResponse:
@@ -30,6 +49,7 @@ class ModelRepositoryServicer(ModelRepositoryServiceServicer):
         return mr_pb.RepositoryModelLoadResponse()
 
     @handle_mlserver_error
+    @_deprecated
     async def RepositoryModelUnload(
         self, request: mr_pb.RepositoryModelUnloadRequest, context
     ) -> mr_pb.RepositoryModelUnloadResponse:
