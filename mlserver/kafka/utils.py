9cb3496cac83d21352937d196c69d79efea57897
Adrian Gonzalez-Martin <agm@seldon.io>
2022-06-09 18:03:02 +0100

    Fix linter

--- mlserver/kafka/utils.py
+++ mlserver/kafka/utils.py
@@ -1,7 +1,7 @@
 import json
 
 from pydantic import BaseModel
-from typing import Dict, Tuple, List
+from typing import Dict, Tuple, List, Union
 
 try:
     import orjson
@@ -9,15 +9,16 @@ except ImportError:
     orjson = None  # type: ignore
 
 
-def encode_value(v: BaseModel) -> str:
+def encode_value(v: BaseModel) -> bytes:
     as_dict = v.dict()
     if orjson is None:
-        return json.dumps(as_dict)
+        dumped = json.dumps(as_dict)
+        return dumped.encode("utf-8")
 
     return orjson.dumps(as_dict)
 
 
-def decode_value(v: bytes) -> dict:
+def decode_value(v: Union[bytes, str]) -> dict:
     if orjson is None:
         return json.loads(v)
 
