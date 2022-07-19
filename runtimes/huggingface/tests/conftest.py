4561c96dd37c9ff9a1a693b6d652de84affbb6d6
Adrian Gonzalez-Martin <agm@seldon.io>
2022-07-19 11:08:10 +0200

    Fix linter

--- runtimes/huggingface/tests/conftest.py
+++ runtimes/huggingface/tests/conftest.py
@@ -7,6 +7,7 @@ from mlserver.settings import ModelSettings, ModelParameters
 
 from mlserver_huggingface import HuggingFaceRuntime
 
+
 @pytest.fixture(scope="module")
 def event_loop():
     # NOTE: We need to override the `event_loop` fixture to change its scope to
@@ -17,7 +18,8 @@ def event_loop():
     yield loop
     loop.close()
 
-@pytest.fixture(scope='module')
+
+@pytest.fixture(scope="module")
 def model_settings() -> ModelSettings:
     return ModelSettings(
         name="foo",
@@ -25,14 +27,16 @@ def model_settings() -> ModelSettings:
             extra={
                 "task": "question-answering",
             }
-        )
+        ),
     )
 
-@pytest.fixture(scope='module')
+
+@pytest.fixture(scope="module")
 async def runtime(model_settings: ModelSettings) -> HuggingFaceRuntime:
     runtime = HuggingFaceRuntime(model_settings)
     await runtime.load()
     return runtime
+
 
 @pytest.fixture
 def inference_request() -> InferenceRequest:
@@ -42,13 +46,13 @@ def inference_request() -> InferenceRequest:
                 name="question",
                 shape=[1],
                 datatype="BYTES",
-                data=["what is your name?"]
+                data=["what is your name?"],
             ),
             RequestInput(
                 name="context",
                 shape=[1],
                 datatype="BYTES",
-                data=["Hello, I am Seldon, how is it going"]
+                data=["Hello, I am Seldon, how is it going"],
             ),
         ]
     )
--- runtimes/huggingface/tests/test_runtime.py
+++ runtimes/huggingface/tests/test_runtime.py
@@ -11,7 +11,8 @@ def test_load(runtime: HuggingFaceRuntime):
     assert runtime.ready
     assert isinstance(runtime._model, QuestionAnsweringPipeline)
 
+
 async def test_infer(runtime: HuggingFaceRuntime, inference_request: InferenceRequest):
     res = await runtime.predict(inference_request)
     pred = json.loads(res.outputs[0].data[0])
-    assert pred['answer'] == 'Seldon'
+    assert pred["answer"] == "Seldon"
