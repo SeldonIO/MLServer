009d1c8264991e4b412df4fbfe13b8ae56e3f4d2
Adrian Gonzalez-Martin <agm@seldon.io>
2022-09-23 10:56:36 +0200

    Ensure hf runtime works with batches

--- runtimes/huggingface/tests/test_runtime.py
+++ runtimes/huggingface/tests/test_runtime.py
@@ -16,3 +16,23 @@ async def test_infer(runtime: HuggingFaceRuntime, inference_request: InferenceRe
     res = await runtime.predict(inference_request)
     pred = json.loads(res.outputs[0].data[0])
     assert pred["answer"] == "Seldon"
+
+
+async def test_infer_multiple(
+    runtime: HuggingFaceRuntime, inference_request: InferenceRequest
+):
+    # Send request with two elements
+    for request_input in inference_request.inputs:
+        input_data = request_input.data[0]
+        request_input.data.__root__ = [input_data, input_data]
+        request_input.shape = [2]
+
+    res = await runtime.predict(inference_request)
+
+    assert len(res.outputs) == 1
+
+    response_output = res.outputs[0]
+    assert len(response_output.data) == 2
+    for d in response_output.data:
+        pred = json.loads(d)
+        assert pred["answer"] == "Seldon"
