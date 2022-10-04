4692494f7b68e539f62ca7a0421f298167dd3d0d
Adrian Gonzalez-Martin <agm@seldon.io>
2022-10-04 16:24:22 +0100

    Fix linter

--- mlserver/codecs/decorator.py
+++ mlserver/codecs/decorator.py
@@ -19,11 +19,11 @@ except ImportError:
     # The `get_origin` and `get_args` don't exist in Python 3.7, so we'll
     # backfill them manually.
     # When we remove support for Python 3.7, remove these methods.
-    def get_origin(t: Type) -> Type:
+    def get_origin(t: Type) -> Type:  # type: ignore
         return getattr(t, "__origin__", type(None))
 
-    def get_args(t: Type) -> List[Type]:
-        return getattr(t, "__args__", [])
+    def get_args(t: Type) -> tuple:  # type: ignore
+        return getattr(t, "__args__", ())
 
 
 from ..types import InferenceRequest, InferenceResponse, ResponseOutput
