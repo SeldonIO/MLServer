5e6829827352921aee0b72d4dc04164c80c74d29
Adrian Gonzalez-Martin <agm@seldon.io>
2022-10-07 17:01:46 +0100

    Move summodel to local folder

--- benchmarking/testserver/models/sum-model/models.py
+++ benchmarking/testserver/models/sum-model/models.py
@@ -1,8 +1,12 @@
 from mlserver import types, MLModel
+from mlserver.logging import logger
 
 
 class SumModel(MLModel):
     async def predict(self, payload: types.InferenceRequest) -> types.InferenceResponse:
+        logger.debug("printin things")
+        logger.info("printin more things (as info)")
+        logger.warning("now this is a warning!")
         total = 0
         for inp in payload.inputs:
             total += sum(inp.data)
