01818f3ba28c660ec44cb03716636bf5f3449615
Clive Cox <cc@seldon.io>
2022-10-11 14:52:46 +0100

    lint

--- runtimes/alibi-explain/mlserver_alibi_explain/common.py
+++ runtimes/alibi-explain/mlserver_alibi_explain/common.py
@@ -53,7 +53,7 @@ def convert_from_bytes(output: ResponseOutput, ty: Optional[Type] = None) -> Any
 
 # TODO: add retry and better exceptions handling
 def remote_predict(
-    v2_payload: InferenceRequest, predictor_url: str, ssl_verify_path: str
+    v2_payload: InferenceRequest, predictor_url: str, ssl_verify_path: Union[str, bool]
 ) -> InferenceResponse:
     verify = True
     if ssl_verify_path != "":
@@ -64,7 +64,9 @@ def remote_predict(
     return InferenceResponse.parse_raw(response_raw.text)
 
 
-def remote_metadata(url: str, ssl_verify_path: str) -> MetadataModelResponse:
+def remote_metadata(
+    url: str, ssl_verify_path: Union[str, bool]
+) -> MetadataModelResponse:
     """Get metadata from v2 endpoint"""
     verify = True
     if ssl_verify_path != "":
