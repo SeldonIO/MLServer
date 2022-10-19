61d288d2322c9c6f28fce34d10ed8c549ce757af
adriangonz <adriangonz@users.noreply.github.com>
2022-10-19 09:52:34 +0000

    Bump version to 1.2.0.dev10

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ html_title = "MLServer Documentation"
 author = "Seldon Technologies"
 
 # The full version, including alpha/beta/rc tags
-release = "1.2.0.dev9"
+release = "1.2.0.dev10"
 
 
 # -- General configuration ---------------------------------------------------
