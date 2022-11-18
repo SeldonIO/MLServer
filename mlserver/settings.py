65663c77e96e47b03966ef0081a6b7e2c76c4153
jgallardorama <javiergram@ext.inditex.com>
2022-11-18 09:40:19 +0100

    Review 2022-11-18

--- mlserver/settings.py
+++ mlserver/settings.py
@@ -90,9 +90,14 @@ class Settings(BaseSettings):
     model_repository_root: str = "."
     """Root of the model repository, where we will search for models."""
 
+    # Custom model repository class implementation
     model_repository_implementation: Optional[PyObject]
+    """*Python path* to the inference runtime to model repository (e.g.
+    ``mlserver.repository.repository.SchemalessModelRepository``)."""
 
+    # Model Repository parameters are meant to be set directly by the MLServer runtime.
     model_repository_implementation_args: dict = {}
+    """Extra parameters for model repository."""
 
     load_models_at_startup: bool = True
     """Flag to load all available models automatically at startup."""
