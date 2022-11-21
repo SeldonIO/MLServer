26b8c57152f639cf1c37973384cbc2df35a9826c
Adrian Gonzalez-Martin <agm@seldon.io>
2022-11-21 15:46:22 +0000

    Add dep bounds in HF runtime

--- runtimes/huggingface/setup.py
+++ runtimes/huggingface/setup.py
@@ -36,9 +36,9 @@ setup(
     install_requires=[
         "mlserver",
         "optimum[onnxruntime]>=1.2.3",
-        # Pin `transformers`, otherwise we risk falling into this issue:
+        # Avoid `transformers` versions incompatible with Optimum:
         # https://github.com/huggingface/optimum/issues/344
-        "transformers<=4.21.1",
+        "transformers <=4.21.1, >=4.22.0",
     ],
     long_description=_load_description(),
     long_description_content_type="text/markdown",
