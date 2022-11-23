9ef96bab9eef696449c8d2ccf785704097699ac0
Adrian Gonzalez-Martin <agm@seldon.io>
2022-11-23 16:49:25 +0000

    Log plain exceptions in gRPC error handler

--- mlserver/grpc/utils.py
+++ mlserver/grpc/utils.py
@@ -43,7 +43,10 @@ def handle_mlserver_error(f: Callable):
         try:
             return await f(self, request, context)
         except MLServerError as err:
-            logger.error(err)
+            logger.exception(err)
             await context.abort(code=_grpc_status_code(err), details=str(err))
+        except Exception as err:
+            logger.exception(err)
+            await context.abort(code=grpc.StatusCode.INTERNAL, details=str(err))
 
     return _inner
