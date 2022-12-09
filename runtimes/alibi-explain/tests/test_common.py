a7aeb992fbaa4cc8a5e8be3a1dd5573b8526d9d4
Adrian Gonzalez-Martin <agm@seldon.io>
2022-12-09 15:00:21 +0000

    Send "don't log" header for Alibi's internal requests (#890)
    Fix https://github.com/SeldonIO/MLServer/issues/880

--- runtimes/alibi-explain/tests/test_common.py
+++ runtimes/alibi-explain/tests/test_common.py
@@ -1,8 +1,17 @@
 import pytest
 
-from mlserver.types import ResponseOutput
+from mlserver.types import (
+    InferenceRequest,
+    InferenceResponse,
+    RequestInput,
+    ResponseOutput,
+)
 
-from mlserver_alibi_explain.common import convert_from_bytes
+from mlserver_alibi_explain.common import (
+    convert_from_bytes,
+    remote_predict,
+    SELDON_SKIP_LOGGING_HEADER,
+)
 from mlserver_alibi_explain.errors import InvalidExplanationShape
 
 
@@ -16,3 +25,24 @@ from mlserver_alibi_explain.errors import InvalidExplanationShape
 def test_convert_from_bytes_invalid(output: ResponseOutput):
     with pytest.raises(InvalidExplanationShape):
         convert_from_bytes(output)
+
+
+def test_remote_predict(requests_mock):
+    uri = "mock://v2/models/foo/infer"
+
+    inference_request = InferenceRequest(
+        inputs=[RequestInput(name="bar", datatype="INT32", shape=[1, 2], data=[1, 2])]
+    )
+    inference_response = InferenceResponse(
+        model_name="foo",
+        outputs=[ResponseOutput(name="bar2", datatype="FP32", shape=[1, 1], data=[3])],
+    )
+
+    requests_mock.post(
+        uri,
+        text=inference_response.json(),
+        request_headers={SELDON_SKIP_LOGGING_HEADER: "true"},
+    )
+    res = remote_predict(inference_request, uri, True)
+
+    assert res == inference_response
