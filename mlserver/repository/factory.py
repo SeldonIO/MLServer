09d7d9e6292d3335fc99bb4abcb8076239a9fc29
jgallardorama <javiergram@ext.inditex.com>
2022-12-13 09:46:54 +0100

    Fix: error: "object" not callable

--- mlserver/repository/factory.py
+++ mlserver/repository/factory.py
@@ -1,15 +1,18 @@
 from .repository import ModelRepository, SchemalessModelRepository
 from ..settings import Settings
+from pydantic import PyObject
 
 
 class ModelRepositoryFactory:
     @staticmethod
     def resolve_model_repository(settings: Settings) -> ModelRepository:
-        result: ModelRepository
-        if not settings.model_repository_implementation:
-            settings.model_repository_implementation = SchemalessModelRepository
+        model_repository_implementation: PyObject = SchemalessModelRepository
 
-        result = settings.model_repository_implementation(
+        result: ModelRepository
+        if settings.model_repository_implementation:
+            model_repository_implementation = settings.model_repository_implementation
+
+        result = model_repository_implementation(
             root=settings.model_repository_root,
             **settings.model_repository_implementation_args,
         )
