9bb2d99141632776a097c1b1e46179212089ab19
Adrian Gonzalez-Martin <agm@seldon.io>
2023-03-09 14:20:09 +0000

    Revert version back to 1.2.3 to ensure tag gets created

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ html_title = "MLServer Documentation"
 author = "Seldon Technologies"
 
 # The full version, including alpha/beta/rc tags
-release = "2.x-dep"
+release = "1.2.4"
 
 
 # -- General configuration ---------------------------------------------------
