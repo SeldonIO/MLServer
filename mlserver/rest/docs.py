a06b67e7df98b105e402a56014eccff4c0ab16eb
Adrian Gonzalez-Martin <agm@seldon.io>
2023-04-04 11:03:33 +0100

    Cache openapi schema

--- mlserver/rest/docs.py
+++ mlserver/rest/docs.py
@@ -1,10 +1,13 @@
 import orjson
 
+from functools import lru_cache
+from typing import Optional
 from importlib_resources import files
 
 OPENAPI_SCHEMA_RELATIVE_PATH = "../openapi/dataplane.json"
 
 
+@lru_cache
 def get_openapi_schema() -> dict:
     mlserver_package = __package__.split(".")[0]
     openapi_schema_path = files(mlserver_package).joinpath(OPENAPI_SCHEMA_RELATIVE_PATH)
