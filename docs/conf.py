122fa71ca1d72d254a510eceb78f71f601abb0c1
adriangonz <adriangonz@users.noreply.github.com>
2023-04-06 15:38:31 +0000

    Bump version to 1.3.0.rc2

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ html_title = "MLServer Documentation"
 author = "Seldon Technologies"
 
 # The full version, including alpha/beta/rc tags
-release = "1.3.0.rc1"
+release = "1.3.0.rc2"
 
 
 # -- General configuration ---------------------------------------------------
