67702ba32243171ccdf5aebf4cf67474d3c3ea03
Adrian Gonzalez-Martin <agm@seldon.io>
2023-06-15 15:54:13 +0100

    Add simple registry to keep track of which worker has each models

--- mlserver/parallel/pool.py
+++ mlserver/parallel/pool.py
@@ -5,7 +5,7 @@ from typing import Awaitable, Callable, Dict, Optional, List
 
 from ..model import MLModel
 from ..types import InferenceRequest, InferenceResponse
-from ..settings import Settings
+from ..settings import Settings, ModelSettings
 from ..env import Environment
 
 from .model import ParallelModel
@@ -21,6 +21,37 @@ from .dispatcher import Dispatcher
 
 PredictMethod = Callable[[InferenceRequest], Awaitable[InferenceResponse]]
 InferencePoolHook = Callable[[Worker], Awaitable[None]]
+
+
+class WorkerRegistry:
+    """
+    Simple registry to keep track of which models have been loaded.
+    This can be used to re-load all models when a worker stops unexpectedly.
+    """
+
+    def __init__(self):
+        self._models: Dict[str, ModelSettings] = {}
+
+    def _key(self, model_settings: ModelSettings) -> str:
+        return f"{model_settings.name}-{model_settings.version}"
+
+    def on_model_load(self, model_settings: ModelSettings):
+        model_key = self._key(model_settings)
+        self._models[model_key] = model_settings
+
+    def on_model_reload(
+        self, old_model_settings: ModelSettings, new_model_settings: ModelSettings
+    ):
+        self.on_model_unload(old_model_settings)
+        self.on_model_load(new_model_settings)
+
+    def on_model_unload(self, model_settings: ModelSettings):
+        model_key = self._key(model_settings)
+        if model_key in self._models:
+            del self._models[model_key]
+
+    def __len__(self) -> int:
+        return len(self._models)
 
 
 class InferencePool:
@@ -42,22 +73,19 @@ class InferencePool:
     ):
         configure_inference_pool(settings)
 
-        # TODO: Hook on_worker_stop to "worker unexpectedly died" event
         self._on_worker_stop = on_worker_stop
         self._env = env
         self._workers: Dict[int, Worker] = {}
+        self._worker_registry = WorkerRegistry()
         self._settings = settings
         self._responses: Queue[ModelResponseMessage] = Queue()
         for idx in range(self._settings.parallel_workers):
-            # TODO: Set callback to restart worker if it goes down (would
-            # `worker.join` help with that?)
             worker = Worker(self._settings, self._responses, self._env)
             worker.start()
             self._workers[worker.pid] = worker  # type: ignore
 
         self._dispatcher = Dispatcher(self._workers, self._responses)
         self._dispatcher.start()
-        self._model_count = 0
 
     @property
     def env_hash(self) -> Optional[str]:
@@ -66,6 +94,21 @@ class InferencePool:
 
         return self._env.env_hash
 
+    def on_worker_stop(self, pid: int, exit_code: int):
+        if pid not in self._workers:
+            # If this worker didn't belong to this pool, ignore
+            return
+
+        # TODO: Call downstream _on_worker_stop hooks
+        worker = self._workers[pid]
+        self._dispatcher.on_worker_stop(worker, exit_code)
+        if pid in self._workers:
+            # NOTE: worker may be removed by dispatcher
+            del self._workers[pid]
+
+        #  worker = Worker(self._settings, self._responses, self._env)
+        #  worker.start()
+
     async def load_model(self, model: MLModel) -> MLModel:
         load_message = ModelUpdateMessage(
             update_type=ModelUpdateType.Load,
@@ -73,12 +116,13 @@ class InferencePool:
         )
         await self._dispatcher.dispatch_update(load_message)
 
-        self._model_count += 1
+        self._worker_registry.on_model_load(model.settings)
         return ParallelModel(model, self._dispatcher)
 
     async def reload_model(self, old_model: MLModel, new_model: MLModel) -> MLModel:
         # The model registries within each worker will take care of reloading
         # the model internally
+        self._worker_registry.on_model_reload(old_model.settings, new_model.settings)
         return await self.load_model(new_model)
 
     async def unload_model(self, model: MLModel) -> MLModel:
@@ -88,11 +132,11 @@ class InferencePool:
         )
         await self._dispatcher.dispatch_update(unload_message)
 
-        self._model_count -= 1
+        self._worker_registry.on_model_unload(model.settings)
         return ParallelModel(model, self._dispatcher)
 
     def empty(self) -> bool:
-        return self._model_count == 0
+        return len(self._worker_registry) == 0
 
     async def close(self):
         await self._close_workers()
