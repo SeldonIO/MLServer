7b4bbeb443c2a6f1670ba12e9ca8d1c8f1015539
Uxito-Ada <414416158@qq.com>
2023-07-04 13:08:44 +0800

    overwrite lgbm load with spark API

--- runtimes/lightgbm/mlserver_lightgbm/lightgbm.py
+++ runtimes/lightgbm/mlserver_lightgbm/lightgbm.py
@@ -5,6 +5,9 @@ from mlserver.model import MLModel
 from mlserver.utils import get_model_uri
 from mlserver.codecs import NumpyCodec, NumpyRequestCodec
 
+from bigdl.ppml.ppml_context import *
+from bigdl.ppml.kms.utils.kms_argument_parser import KmsArgumentParser
+from pyspark import SparkConf
 
 WELLKNOWN_MODEL_FILENAMES = ["model.bst"]
 
@@ -19,7 +22,15 @@ class LightGBMModel(MLModel):
             self._settings, wellknown_filenames=WELLKNOWN_MODEL_FILENAMES
         )
 
-        self._model = lgb.Booster(model_file=model_uri)
+        # init a PPMLContext instance
+        args = KmsArgumentParser().get_arg_dict()
+        conf = SparkConf().set("spark.driver.host", "127.0.0.1")
+        sc = PPMLContext(app_name='mlserver-lightgbm-serving',\
+                ppml_args=args, spark_conf=conf)
+
+        # use PPMLContext to load encrypted/plaintext lightgbm model
+        self._model = sc.loadLightGBMClassificationModel(\
+                model_path=model_uri, crypto_mode=args["input_encrypt_mode"])
 
         return True
 
