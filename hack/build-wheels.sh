2f274443ed3ecbfa395fb3896b92d1ec8eac47b1
Lucian Carata <lc525@users.noreply.github.com>
2023-07-11 10:46:39 +0100

    Fix build-wheels.sh error when copying to output path (#1286)

--- hack/build-wheels.sh
+++ hack/build-wheels.sh
@@ -15,24 +15,29 @@ fi
 _buildWheel() {
   local _srcPath=$1
   local _outputPath=$2
-  local _currentDir=$PWD
 
   # Poetry doesn't let us send the output to a separate folder so we'll `cd`
   # into the folder and them move the wheels out
   # https://github.com/python-poetry/poetry/issues/3586
   pushd $_srcPath
   poetry build
-  cp ./dist/* $_outputPath
+  # Only copy files if destination is different from source
+  local _currentDistPath=$PWD/dist
+  if ! [[ "$_currentDistPath" = "$_outputPath" ]]; then
+    cp $_currentDistPath/* $_outputPath
+  fi
   popd
 }
 
 _main() {
   # Convert any path into an absolute path
   local _outputPath=$1
-  if ! [[ "$_outputPath" = /* ]]; then
-    _outputPath="$PWD/$_outputPath"
-  fi
   mkdir -p $_outputPath
+  if ! [[ "$_outputPath" = /* ]]; then
+    pushd $_outputPath
+    _outputPath="$PWD"
+    popd
+  fi
 
   # Build MLServer
   echo "---> Building MLServer wheel"
