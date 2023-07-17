05d32282ae407a17bf0d0c83e9080e63f1d1c91a
Viktor Taskov <viktor.taskov@seldon.io>
2023-07-17 15:55:41 +0100

    602 Move logging test to package

--- tests/logging/test_logging.py
+++ tests/logging/test_logging.py
@@ -2,13 +2,10 @@ import pytest
 
 from mlserver import ModelSettings
 from mlserver.context import model_context
-from mlserver.logging import ModelLoggerFormatter, LoggerFormat
-from mlserver.settings import ModelParameters
+from mlserver.logging import ModelLoggerFormatter, LoggerFormat, configure_logger, logger
+from mlserver.settings import ModelParameters, Settings
 from tests.fixtures import SumModel
-from logging import getLogger, INFO
-
-
-logger = getLogger("test.formatter")
+from logging import INFO
 
 
 @pytest.mark.parametrize(
@@ -31,7 +28,7 @@ logger = getLogger("test.formatter")
         ),
     ],
 )
-def test_model_logging_formatter(caplog, name, version, expected_fmt):
+def test_model_logging_formatter(name, version, expected_fmt, caplog):
     caplog.handler.setFormatter(ModelLoggerFormatter(LoggerFormat))
     caplog.set_level(INFO)
 
@@ -50,3 +47,17 @@ def test_model_logging_formatter(caplog, name, version, expected_fmt):
     assert expected_fmt not in log_records[0]
     assert expected_fmt in log_records[1]
     assert expected_fmt not in log_records[2]
+
+
+@pytest.mark.parametrize("debug", [True, False])
+def test_log_level_gets_persisted(debug: bool, settings: Settings, caplog):
+    settings.debug = debug
+    configure_logger(settings)
+
+    test_log_message = "foo - bar - this is a test"
+    logger.debug(test_log_message)
+
+    if debug:
+        assert test_log_message in caplog.text
+    else:
+        assert test_log_message not in caplog.text
