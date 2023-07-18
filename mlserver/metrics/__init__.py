4f27dcae2a7e98eaaf63b5025095f1086974eb3e
Viktor Taskov <viktor.taskov@seldon.io>
2023-07-18 13:15:27 +0100

    602 Flatten test logging folder and remove redundant import

--- mlserver/metrics/__init__.py
+++ mlserver/metrics/__init__.py
@@ -1,13 +1,11 @@
 from .server import MetricsServer
 from .prometheus import configure_metrics
 from .context import register, log
-from ..context import model_context
 from .registry import REGISTRY
 
 __all__ = [
     "MetricsServer",
     "configure_metrics",
-    "model_context",
     "register",
     "log",
     "REGISTRY",
