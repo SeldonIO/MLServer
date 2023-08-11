5c559515d963fbfeff36cf4a4607b17afbeb4ef8
Viktor Taskov <viktor.taskov@seldon.io>
2023-08-11 17:39:27 +0100

    Revert change out of scope

--- mlserver/repository/factory.py
+++ mlserver/repository/factory.py
@@ -8,6 +8,7 @@ class ModelRepositoryFactory:
     def resolve_model_repository(settings: Settings) -> ModelRepository:
         model_repository_implementation: PyObject = SchemalessModelRepository
 
+        result: ModelRepository
         if settings.model_repository_implementation:
             model_repository_implementation = settings.model_repository_implementation
 
@@ -15,4 +16,5 @@ class ModelRepositoryFactory:
             root=settings.model_repository_root,
             **settings.model_repository_implementation_args,
         )
+
         return result
