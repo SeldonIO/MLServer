f6a39011173dbf400257c00e14f673cf304698cf
Jake Bauer <j.bauer@striveworks.us>
2023-12-12 19:35:10 +0000

    linted

--- runtimes/huggingface/tests/test_tasks/test_japanese_mask_fill.py
+++ runtimes/huggingface/tests/test_tasks/test_japanese_mask_fill.py
@@ -1,5 +1,4 @@
 import pytest
-from mlserver.types import InferenceRequest, RequestInput, Parameters
 from mlserver.settings import ModelSettings, ModelParameters
 from mlserver_huggingface import HuggingFaceRuntime
 from mlserver_huggingface.codecs import HuggingfaceRequestCodec
@@ -7,10 +6,11 @@ from mlserver_huggingface.codecs import HuggingfaceRequestCodec
 
 test_sentence = "実際に空が[MASK]のか？"
 
+
 @pytest.fixture
-def current_model_settings():
-    # if not request.config.option.test_hg_tasks:
-    #     pytest.skip()
+def current_model_settings(request) -> ModelSettings:
+    if not request.config.option.test_hg_tasks:
+        pytest.skip()
     return ModelSettings(
         name="model",
         implementation=HuggingFaceRuntime,
@@ -23,25 +23,26 @@ def current_model_settings():
         ),
     )
 
+
 @pytest.fixture
 async def current_runtime(current_model_settings: ModelSettings) -> HuggingFaceRuntime:
     runtime = HuggingFaceRuntime(current_model_settings)
     await runtime.load()
     return runtime
 
+
 @pytest.fixture
 def japanese_text_request():
-    inputs = [
-        test_sentence
-    ]
+    inputs = [test_sentence]
     req = HuggingfaceRequestCodec.encode_request(
-        {"inputs" : inputs},
+        {"inputs": inputs},
         use_bytes=False,
     )
     return req
+
 
 async def test_infer(current_runtime, japanese_text_request):
     resp = await current_runtime.predict(japanese_text_request)
     decoded = HuggingfaceRequestCodec.decode_response(resp)
     top_result = list(decoded.items())[0][1][0]
-    assert top_result['token_str'] == "見える", "Inference is incorrect!"
+    assert top_result["token_str"] == "見える", "Inference is incorrect!"
