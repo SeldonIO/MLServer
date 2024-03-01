6cbf1e56e4510455f7f2b365fe530b7df293ce7b
Jesse Claven <jesse.claven@me.com>
2024-03-01 10:07:50 +0000

    build: Bump version to match release

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ html_title = "MLServer Documentation"
 author = "Seldon Technologies"
 
 # The full version, including alpha/beta/rc tags
-release = "1.4.0.dev11"
+release = "1.4.0"
 
 
 # -- General configuration ---------------------------------------------------
