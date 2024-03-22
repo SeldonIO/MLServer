c229b1f8439e57226726853e15d2f1775fa9ea6b
Nanbo Liu <nliu@lambda002.america.striveworks.us>
2024-03-22 16:18:46 +0000

    rerun lint

--- runtimes/huggingface/mlserver_huggingface/pipelines.py
+++ runtimes/huggingface/mlserver_huggingface/pipelines.py
@@ -24,7 +24,6 @@ class StEmbeddingPipeline(Pipeline):
             self.model_name
         ), f"{self.model_name} is not a sentence transformers model."
         self.model = SentenceTransformer(self.model_name)
-
     def _sanitize_parameters(self, **kwargs):
         forward_kwargs = {}
         if "prompt_name" in kwargs:
