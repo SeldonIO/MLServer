60b452ac6c399d44567e3d821c40a4392eb9286f
Jesse Claven <jesse.claven@me.com>
2024-05-29 15:20:01 +0100

    build: Move macOS Docker installation to script

---
+++ hack/install-docker-macos.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+# Install Docker
+# From https://github.com/actions/runner-images/issues/17#issuecomment-1537238473
+# From https://github.com/abiosoft/colima/discussions/273#discussioncomment-4959736
+brew install docker docker-buildx
+mkdir -p $HOME/.docker/cli-plugins
+ln -sfn $(which docker-buildx) $HOME/.docker/cli-plugins/docker-buildx
+
+# Install Lima
+brew install lima
+# Don't explicitly start Lima here (e.g. with `limactl start`).
+# Let Colina do that, otherwise, # Colima seems to hang
+# waiting for the SSH requirement to be met.
+
+# Install Colima
+sudo mkdir -p /usr/local/bin
+sudo curl -L -o /usr/local/bin/colima https://github.com/abiosoft/colima/releases/download/v0.5.6/colima-Darwin-x86_64 && sudo chmod +x /usr/local/bin/colima
+# NOTE: Use the `slirp` driver rather than the default to avoid
+# random connection errors.
+# From https://github.com/abiosoft/colima/issues/577
+colima start --memory 5 --network-driver slirp --runtime docker
+
+# Link Colima and Docker
+sudo ln -sf $HOME/.colima/default/docker.sock /var/run/docker.sock
