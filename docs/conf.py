ef3d8ad7c58a5b7135e90ae2ebd463de1a5d202e
RobertSamoilescu <RobertSamoilescu@users.noreply.github.com>
2024-09-10 12:23:48 +0000

    Bump version to 1.6.1.rc2

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ html_title = "MLServer Documentation"
 author = "Seldon Technologies"
 
 # The full version, including alpha/beta/rc tags
-release = "1.6.1.rc1"
+release = "1.6.1.rc2"
 
 
 # -- General configuration ---------------------------------------------------
