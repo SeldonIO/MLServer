b1f8aa3b2aa58de381ad427a9ff1426b7fe063c2
shivakrishnaah <shiva.krishnaah@gmail.com>
2024-11-09 23:00:56 +0530

    Commiting the changes for the gcc and alibi

--- mlserver/cli/constants.py
+++ mlserver/cli/constants.py
@@ -12,7 +12,8 @@ RUN conda config --add channels conda-forge && \\
     conda install conda-libmamba-solver==23.7.0 && \\
     conda config --set solver libmamba && \\
     conda install conda-pack && \\
-    conda install gxx_linux-64 && source activate
+    conda install gcc_linux-aarch64 && \\
+    conda install gxx_linux-aarch64 && source activate
 
 # The `[]` character range will ensure that Docker doesn't complain if the
 # files don't exist:
