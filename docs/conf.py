10c9c2e70bc019efc81ae4fd9adeb6a50b5552d5
sakoush <sakoush@users.noreply.github.com>
2024-11-26 15:27:00 +0000

    Bump version to 1.6.2.rc1

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ html_title = "MLServer Documentation"
 author = "Seldon Technologies"
 
 # The full version, including alpha/beta/rc tags
-release = "1.6.1"
+release = "1.6.2.rc1"
 
 
 # -- General configuration ---------------------------------------------------
