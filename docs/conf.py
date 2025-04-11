6998ea06e88452970b9ab64bb7fd127c6cc45768
RobertSamoilescu <22344480+RobertSamoilescu@users.noreply.github.com>
2025-04-11 13:57:25 +0000

    Bump version to 1.7.0.rc4

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ html_title = "MLServer Documentation"
 author = "Seldon Technologies"
 
 # The full version, including alpha/beta/rc tags
-release = "1.7.0.dev0"
+release = "1.7.0.rc4"
 
 
 # -- General configuration ---------------------------------------------------
