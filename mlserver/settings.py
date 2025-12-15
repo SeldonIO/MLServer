2e866b1bef37e6a423585222d7e599daf1b79300
Lucian Carata <lc525@users.noreply.github.com>
2025-12-15 21:39:49 +0000

    Default MLServer debug setting to False (#2307)
    This means that by default we no longer write the uvicorn accesslog to the
    log (stdout). It makes sense to move to the new default, as production settings
    will not want the verbosity that comes with "debug: True"

--- mlserver/settings.py
+++ mlserver/settings.py
@@ -165,7 +165,7 @@ class Settings(BaseSettings):
         extra="ignore",
     )
 
-    debug: bool = True
+    debug: bool = False
 
     parallel_workers: int = DEFAULT_PARALLEL_WORKERS
     """When parallel inference is enabled, number of workers to run inference
